a0de4ef2bc1771083b382578080ade0d1c8a1535
Bonani Brian Wogqoyi <bbwogqoyi@outlook.com>
2020-07-14 21:41:49 +0200

    Defined the cell and pattern types. Method implementattins: toCells and fromCells

--- Patterns/Library.fs
+++ Patterns/Library.fs
@@ -1,10 +1,51 @@
 ﻿#if !INTERACTIVE
 module Patterns
 #endif
+open System
 
-let toCells v = failwith "Not implemented"
+type Cell = 
+| Positive
+| Negative
+| Unknown
 
-let fromCells v = failwith "Not implemented" 
+type Pattern = 
+| BlackP
+| WhiteP
+| UnknownP
+| Anything
+| EndOfCells
+| ZeroOrMore of Pattern
+| OneOrMore of Pattern
+| Exactly of int*Pattern
+| FewerThan of int*Pattern
+| Sequence of Pattern list
+| Either of Pattern*Pattern
+
+let _charToCell (ch:char) =
+  match (Char.ToLower ch) with
+  |'b' -> Positive
+  |'w' -> Negative
+  |_ -> Unknown
+
+let _cellToChar (cell: Cell) =
+  match cell with
+  | Positive -> 'b'
+  | Negative -> 'w'
+  | Unknown -> '.'
+
+let toCells (str:string)  = 
+  let rec helper (index:int) (out:Cell list) = 
+    match index<(String.length str) with 
+    | false -> out
+    | true -> 
+      let cell = _charToCell str.[index]
+      helper (index+1) (cell::out)
+
+  let unorderedList = helper 0 []
+  List.rev unorderedList
+
+let fromCells (cells: Cell list) =
+  List.map _cellToChar cells
 
 let patternMatch pattern cells = failwith "Not implemented"
 
